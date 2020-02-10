bd133e0b5626ffbb723a0d4eb073d056a00c7ec1
Edalicio Heinzen <edalicio@outlook.com>
2020-02-10 09:33:46 -0300

    Adiciona a APP_PORT

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 app.on('start' , () => {
-  app.listen(3000, () =>  console.log("Servidor iniciado"));
+  app.listen(process.env.APP_PORT 3000, () =>  console.log("Servidor iniciado"));
 })
